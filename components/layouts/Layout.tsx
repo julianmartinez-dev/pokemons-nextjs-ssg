faa187ce28e547f6af997ed69abcd1590602e83c
Julian Martinez <julian.fmartinez@gmail.com>
2022-06-06 22:41:19 -0300

    Se agregan og tags

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -8,25 +8,44 @@ interface LayoutProps {
     children: ReactNode
 }
 
+const origin = (typeof window === "undefined") ? "" : window.location.origin
+
 
 export const Layout : FC<LayoutProps> = ({children, title}) => {
+
+
   return (
     <>
-        <Head>
-            <title>{title || 'Pokemon APP'}</title>
-            <meta name="author" content="Julian Martinez" />
-            <meta name="description" content="Informacion sobre el pokemon X" />
-            <meta name="keywords" content="pokemon, pokedex, informacion, informacion sobre el pokemon x" />
-        </Head>
+      <Head>
+        <title>{title || 'Pokemon APP'}</title>
+        <meta name="author" content="Julian Martinez" />
+        <meta name="description" content="Informacion sobre el pokemon X" />
+        <meta
+          name="keywords"
+          content="pokemon, pokedex, informacion, informacion sobre el pokemon x"
+        />
+        <meta
+          property="og:title"
+          content={`Información sobre el pokemon ${title}`}
+        />
+        <meta
+          property="og:description"
+          content={`Esta es la pagina sobre ${title}`}
+        />
+        <meta
+          property="og:image"
+          content={`${origin}/banner.png`}
+        />
+      </Head>
 
-        <NavBar />
-        <main
-            style={{
-              padding: "0x 20px",
-            }}
-        >
-            {children}
-        </main>
+      <NavBar />
+      <main
+        style={{
+          padding: '0x 20px',
+        }}
+      >
+        {children}
+      </main>
     </>
-  )
+  );
 }
